397b14bb76d7057d02f68b2492eca1eb5a4c2190
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-17 19:38:34 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,14 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Admin Klaim Asuransi</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="css/dashboard/style.css" />
 </head>
-<body class="bg-gray-100">
-    <div class="container mx-auto p-6">
-        <h1 class="text-3xl font-bold mb-6">Dashboard Admin Klaim Asuransi</h1>
+
+<body>
+    <aside>
+        <div class="container">
+            <!-- Sidebar -->
+            <div class="logo">
+                <img src="" alt="logo">
+                <h2>Insura</h2>
+            </div>
+            <div class="close" id="close-btn">
+                <span class="material-symbols-outlined">
+                    close
+                </span>
+            </div>
+            <div class="sidebar">
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        dashboard
+                    </span>
+                    <h3>Dashboard</h3>
+                </a>
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        person_outline
+                    </span>
+                    <h3>Admin</h3>
+                </a>
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        receipt_long
+                    </span>
+                    <h3>Klaim</h3>
+                </a>
+                <a href="#" class="active">
+                    <span class="material-symbols-outlined">
+                        insights
+                    </span>
+                    <h3>Laporan</h3>
+                </a>
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        settings
+                    </span>
+                    <h3>Pengaturan</h3>
+                </a>
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        help
+                    </span>
+                    <h3>Bantuan</h3>
+                </a>
+                <a href="#">
+                    <span class="material-symbols-outlined">
+                        logout
+                    </span>
+                    <h3>LogOut</h3>
+                </a>
+            </div>
+    </aside>
+
+    </div>
+
+
+
 
 </body>
+
 </html>
