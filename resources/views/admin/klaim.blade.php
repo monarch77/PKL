4d7456711dda1b3df9d261790e8e564fae3112cb
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-17 23:26:08 +0700

    Update klaim.blade.php

--- resources/views/admin/klaim.blade.php
+++ resources/views/admin/klaim.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Klaim</title>
-</head>
-<body>
-    <h1>Ini Halaman Klaim</h1>
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'Klaim Asuransi')
+
+@section('content')
+    <h1>Halaman Klaim</h1>
+    <p>Ini adalah halaman untuk mengurus klaim asuransi</p>
+@endsection
