c3b062c52b5ef1de791673fa98778f13ce3b8a95
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-17 23:26:05 +0700

    Update laporan.blade.php

--- resources/views/admin/laporan.blade.php
+++ resources/views/admin/laporan.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laporan</title>
-</head>
-<body>
-    <h1>Ini Halaman Laporan</h1>
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'Laporan Asuransi')
+
+@section('content')
+<h1>Halaman Laporan Asuransi</h1>
+<p>Ini adalah halaman untuk mengelola laporan asuransi.</p>
+@endsection
