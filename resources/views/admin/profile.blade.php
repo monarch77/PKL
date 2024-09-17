2666bbec77ab4e5a7dd4c4d56a91fbbb9fb6c3d1
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-17 23:26:03 +0700

    Update profile.blade.php

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile Admin</title>
-</head>
-<body>
-    <h1>Ini Halaman Profile</h1>
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'Profile Admin')
+
+@section('content')
+<h1>Halaman Profile Admin</h1>
+<p>Ini adalah halaman untuk mengelola profil admin.</p>
+@endsection
