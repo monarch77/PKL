83cae7ae84b0c8b0ff0685ccefcf0f5d90cb9895
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-17 16:41:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+//login
 Route::get('/', function () {
     return view('login');
+});
+
+//admin
+Route::get('/dashboardadmin', function () {
+    return view('admin/dashboard');
+});
+
+
+//user
+Route::get('/dashboarduser', function () {
+    return view('user/dashboard');
 });
