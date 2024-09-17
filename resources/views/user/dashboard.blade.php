bbfed2d21c74ef0fdf3895eb127ad68cf2864bc5
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-17 16:41:31 +0700

    Membuat halaman Dashboard User

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - User Klaim Asuransi</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+    <div class="container mx-auto p-6">
+        <h1 class="text-3xl font-bold mb-6">Dashboard User Klaim Asuransi</h1>
+
+</body>
+</html>
