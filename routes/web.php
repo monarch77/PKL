49af12065b1ea76f87ad7d333153e811564f0d25
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-17 20:19:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,25 @@ Route::get('/dashboardadmin', function () {
     return view('admin/dashboard');
 });
 
+Route::get('/dashboardadmin/profile', function () {
+    return view('admin/profile');
+});
+
+Route::get('/dashboardadmin/laporan', function () {
+    return view('admin/laporan');
+});
+
+Route::get('/dashboardadmin/klaim', function () {
+    return view('admin/klaim');
+});
+
 
 //user
 Route::get('/dashboarduser', function () {
     return view('user/dashboard');
+});
+
+//logout
+Route::get('/logout', function () {
+    return view('login');
 });
