7584ec96401a15bd9236ad2aad89d416d40f3bcf
Aditya Suryandaru <91068712+monarch77@users.noreply.github.com>
2024-09-20 15:38:29 +0700

    Create pengajuan_klaim.blade.php

---
+++ resources/views/user/pengajuan_klaim.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.sidebar_user')
+
+@section('title', 'Dashboard User')
+
+@section('content')
+<h1>Halaman Ajukan Klaim</h1>
+<p>Ini adalah halaman Pengajuan Klaim.</p>
+@endsection
